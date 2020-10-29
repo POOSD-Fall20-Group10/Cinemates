aabd77a69ba23b00c2f64a2e383f441f66525ee9
StyxenStones <46583406+StyxenStones@users.noreply.github.com>
2020-10-29 00:16:18 -0400

    Added Main.js

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+const app_name = 'cine-mates'
+function buildPath(route)
+{
+    if (process.env.NODE_ENV === 'production')
+    {
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
+    }
+    else
+    {
+        return 'http://localhost:5000/' + route;
+    }
+}
+
+
+function Main()
+{
+
+    return(
+    <div id="mainDiv">
+    </div>
+    );
+};
+
+export default Main;
