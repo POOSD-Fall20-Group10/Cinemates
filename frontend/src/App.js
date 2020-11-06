665e103e5903197cd33431e5fd95efb7c565dc37
cg021 <christiang021@gmail.com>
2020-11-05 20:22:04 -0600

    add new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,53 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch, Link } from 'react-router-dom';
+
+import './bootstrap.min.css';
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MainPage from './pages/MainPage';
 
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+
 function App() {
   return (
-    <Router >
-      <Switch>
+  <Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/"}>Login</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/register"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+        <Switch>
         <Route path="/" exact>
-          <LoginPage />
+          <Login />
         </Route>
         <Route path="/register" exact>
-          <RegisterPage />
+          <Register />
         </Route>
         <Route path="/main" exact>
           <MainPage />
         </Route>
         <Redirect to="/" />
       </Switch>
-    </Router>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
