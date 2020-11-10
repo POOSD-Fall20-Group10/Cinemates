6e8034cfd2efc935b213992b9240625ea7996ba2
StyxenStones <46583406+StyxenStones@users.noreply.github.com>
2020-11-09 20:33:59 -0500

    Merge .components

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import './Login.css';
+import React, { useState } from "react";
 
 const app_name = 'cine-mates'
 
@@ -15,64 +14,58 @@ function buildPath(route) {
         return 'http://localhost:5000/' + route;
     }
 }
+    function Login() {
+        var loginName;
+        var loginPassword;
+        const [message, setMessage] = useState('');
 
-function Login() {
-    var loginName;
-    var loginPassword;
-    const [message, setMessage] = useState('');
+        const doLogin = async event => {
+            event.preventDefault();
+            var obj = {login:loginName.value,password:loginPassword.value};
+            var js = JSON.stringify(obj);
 
-    const doLogin = async event => {
-        event.preventDefault();
-        var obj = {login:loginName.value,password:loginPassword.value};
-        var js = JSON.stringify(obj);
+            try {    
+                const response = await fetch(buildPath('api/UserLogin'), {
+                    method:'POST',body:js,headers:{
+                        'Content-Type': 'application/json'
+                    }
+                });
 
-        try {    
-            const response = await fetch(buildPath('api/UserLogin'), {
-                method:'POST',body:js,headers:{
-                    'Content-Type': 'application/json'
-                }
-            });
+            var res = JSON.parse(await response.text());
+            if (res.id <= 0) {
+                setMessage('User/Password combination incorrect');
+            } else {
+                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+                localStorage.setItem('user_data', JSON.stringify(user));
+                setMessage('');
+                window.location.href = '/main';
+              }
+            } catch(e) {
+                alert(e.toString());
+                return;
+              }    
+        };
 
-        var res = JSON.parse(await response.text());
-        if (res.id <= 0) {
-            setMessage('User/Password combination incorrect');
-        } else {
-            var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
-            localStorage.setItem('user_data', JSON.stringify(user));
-            setMessage('');
-            window.location.href = '/main';
-          }
-        } catch(e) {
-            alert(e.toString());
-            return;
-          }    
-    };
+         return (
+            <div>
+                <form onSubmit={doLogin}>
+                    <h3>Login</h3>
 
-    return(
-      <div id="loginDiv" className="Login">
-         <form onSubmit={doLogin}>
-          <div className="input-container">
-          <input type="text" id="loginName" placeholder="Username"
-          ref={(c) => loginName = c} />
-          <i class="zmdi zmdi-account zmdi-hc-lg"></i>
-          </div>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input type="text" className="form-control" id="loginName" placeholder="Enter username" ref={(c) => loginName = c}/>
+                    </div>
 
-          <div className="input-container">
-          <input type="password" id="loginPassword" placeholder="Password"
-          ref={(c) => loginPassword = c} />
-          <i class="zmdi zmdi-lock zmdi-hc-lg"></i>
-          </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" id="loginPassword" placeholder="Enter password" ref={(c) => loginPassword = c}/>
+                    </div>
 
-          <input type="submit" id="loginButton" class="buttons" value = "Login"
-          onClick={doLogin} />
-
-          <input type="submit" id="registerButton" class="buttons" value = "Register"
-          onClick={doRegisterButton} />
-
-         </form>
-        <span id="loginResult">{message}</span>
-      </div>
-    );
-}
-
-export default Login;
+                    <button type="submit" className="btn btn-dark btn-lg btn-block" id="loginButton" onClick={doLogin}>Sign in</button>
+                </form>
+                <span id="loginResult">{message}</span>
+            </div>
+        );
+    }
+    
+    export default Login;
