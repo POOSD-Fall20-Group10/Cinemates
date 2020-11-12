ef7972b27edd115514f243db3825ad2076119059
Robert Moore <robbiemoore4698@gmail.com>
2020-11-11 19:51:20 -0500

    references now consistently use plain strings, not ObjectIDs

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.post('/API/AddUserToGroup', async (req, res, next) =>
 
   const db = client.db();
 
-  db.collection('groups').update({_id: new mongo.ObjectID(groupID)},{ $addToSet: {members: {userID : mongo.ObjectID(userID), yesList : [], noList : []}}});
+  db.collection('groups').update({_id: new mongo.ObjectID(groupID)},{ $addToSet: {members: {userID : userID, yesList : [], noList : []}}});
 
   var ret = { error: error };
   res.status(200).json(ret);
@@ -211,7 +211,7 @@ app.post('/API/DeleteUserFromGroup', async (req, res, next) =>
 
   const db = client.db();
 
-  db.collection('groups').update({_id: new mongo.ObjectID(groupID)},{ $pull: {members: {"userID" : new mongo.ObjectID(userID)}}});
+  db.collection('groups').update({_id: new mongo.ObjectID(groupID)},{ $pull: {members: {"userID" : userID}}});
 
   var ret = { error: error };
   res.status(200).json(ret);
@@ -225,7 +225,7 @@ app.post('/API/ListGroups', async (req, res, next) =>
   const { userID } = req.body;
 
   const db = client.db();
-  const results = await db.collection('groups').find({"members.userID" : new mongo.ObjectID(userID)}).toArray();
+  const results = await db.collection('groups').find({"members.userID" : userID}).toArray();
 
   var ret = { groups: results, error:''};
 
@@ -240,12 +240,12 @@ app.post('/API/AddMovieToList', async (req, res, next) =>
 
   const db = client.db();
   if(liked){
-    db.collection('groups').update({_id: new mongo.ObjectID(groupID) , "members.userID" : new mongo.ObjectID(userID)},
-      { $addToSet: {"members.$.yesList" : {movieID : new mongo.ObjectID(movieID)}}});
+    db.collection('groups').update({_id: new mongo.ObjectID(groupID) , "members.userID" : userID},
+      { $addToSet: {"members.$.yesList" : {movieID : movieID}}});
     }
     else{
-      db.collection('groups').update({_id: new mongo.ObjectID(groupID) , "members.userID" : new mongo.ObjectID(userID)},
-        { $addToSet: {"members.$.noList" : {movieID : new mongo.ObjectID(movieID)}}});
+      db.collection('groups').update({_id: new mongo.ObjectID(groupID) , "members.userID" : userID},
+        { $addToSet: {"members.$.noList" : {movieID : movieID}}});
     }
 
   var ret = { error: error };
@@ -312,8 +312,8 @@ app.post('/API/AddFriend', async (req, res, next) =>
 
   const db = client.db();
 
-  db.collection('users').update({_id: new mongo.ObjectID(user1)},{ $addToSet: {friends: {userID : mongo.ObjectID(user2)}}});
-    db.collection('users').update({_id: new mongo.ObjectID(user2)},{ $addToSet: {friends: {userID : mongo.ObjectID(user1)}}});
+  db.collection('users').update({_id: new mongo.ObjectID(user1)},{ $addToSet: {friends: {userID : user2}}});
+    db.collection('users').update({_id: new mongo.ObjectID(user2)},{ $addToSet: {friends: {userID : user1}}});
 
   var ret = { error: error };
   res.status(200).json(ret);
@@ -328,8 +328,8 @@ app.post('/API/DeleteFriend', async (req, res, next) =>
 
   const db = client.db();
 
-  db.collection('users').update({_id: new mongo.ObjectID(user1)},{ $pull: {friends: {userID : mongo.ObjectID(user2)}}});
-    db.collection('users').update({_id: new mongo.ObjectID(user2)},{ $pull: {friends: {userID : mongo.ObjectID(user1)}}});
+  db.collection('users').update({_id: new mongo.ObjectID(user1)},{ $pull: {friends: {userID : user2}}});
+    db.collection('users').update({_id: new mongo.ObjectID(user2)},{ $pull: {friends: {userID : user1}}});
 
   var ret = { error: error };
   res.status(200).json(ret);
@@ -376,12 +376,12 @@ app.post('/API/EmailVerification', async (req, res, next) =>
 {
 
   var error = '';
-  
+
   const { email } = req.body;
-  
+
   const sgMail = require('@sendgrid/mail')
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  
+
   const msg = {
     to: email, // Change to your recipient
     from: 'jesse102999@gmail.com', // Change to your verified sender
@@ -389,7 +389,7 @@ app.post('/API/EmailVerification', async (req, res, next) =>
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   }
-  
+
   sgMail
     .send(msg)
     .then(() => {
