cb697e47239d9f33fc980e40184a02b86b7ab578
StyxenStones <46583406+StyxenStones@users.noreply.github.com>
2020-11-13 02:11:32 -0500

    Added Group and Account Pages

---
+++ frontend/src/pages/AccountPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Account from '../components/Account';
+
+const AccountPage = () =>
+{
+
+    return(
+      <div>
+        <Account />
+      </div>
+    );
+};
+
+export default AccountPage;
