f17e05276a07d323fb595ab463c975c4b77e6397
cg021 <christiang021@gmail.com>
2020-11-13 15:20:44 -0600

    move header divs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,22 +17,6 @@ function App() {
   return (
   <Router>
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/"}>Login</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/register"}>Sign up</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
       <div className="outer">
       <div>&nbsp;&nbsp;&nbsp;</div>
       <div className="container-div">
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -92,6 +92,21 @@ function Header()
 
     return(
         <div id="headerDiv">
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+            <Link className="navbar-brand" to={"/"}>Login</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to={"/"}>Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to={"/register"}>Sign up</Link>
+                </li>
+                </ul>
+            </div>
+            </div>
+            </nav>
             <button type="button" id="AccountButton" class="buttons" onClick={openAccount}> {login} </button>
             <button type="button" id="GroupSelector" class="buttons" onClick={openGroup}> Group </button>
             <button type="button" id="notificationsButton" class="buttons" onClick={openNotifications}> Notifications </button>
