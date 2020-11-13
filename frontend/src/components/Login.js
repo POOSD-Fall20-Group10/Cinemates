320294f1cca0fe56798ba28c0a6734050e155706
cg021 <christiang021@gmail.com>
2020-11-13 15:53:28 -0600

    imports

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Link } from 'react-router-dom';
 const app_name = 'cine-mates'
 
 const doRegisterButton = event => {
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 const app_name = 'cine-mates'
 function buildPath(route)
 {
