0c5bb1180eacad739ea41a2a56cb8f9b594e571f
cg021 <christiang021@gmail.com>
2020-11-13 15:46:19 -0600

    remove nav

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -48,6 +48,21 @@ function buildPath(route) {
 
          return (
             <div>
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                <div className="container">
+                <Link className="navbar-brand" to={"/"}>Login</Link>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to={"/"}>Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to={"/register"}>Sign up</Link>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+                </nav>
                 <form onSubmit={doLogin}>
                     <h3>Login</h3>
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -73,6 +73,21 @@ function Register()
 
     return(
     <div id="regDiv">
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+            <Link className="navbar-brand" to={"/"}>Login</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to={"/"}>Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to={"/register"}>Sign up</Link>
+                </li>
+                </ul>
+            </div>
+            </div>
+            </nav>
             <form onSubmit={doRegister}>
                 <h3>Register</h3>
                 <div className="form-group">
