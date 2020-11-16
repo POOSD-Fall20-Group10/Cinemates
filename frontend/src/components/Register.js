ef9a1a84c8a12d56f12e649b9c0d5484be64cd30
SemiproShark <70351849+SemiproShark@users.noreply.github.com>
2020-11-16 15:59:30 -0500

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -42,7 +42,7 @@ function Register()
             {
 
                 var obj = {email:regEmail.value,login:regLogin.value,
-                    password:CryptoJS.MD5(regPassword.value),firstName:regFName.value,lastName:regLName.value,isVerified:false};
+                    password:CryptoJS.MD5(regPassword.value),firstName:regFName.value,lastName:regLName.value,isVerified:true};
                 var js = JSON.stringify(obj);
 
                 try
