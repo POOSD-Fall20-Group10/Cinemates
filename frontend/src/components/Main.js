c9db105efa3d99dcb083c6c74adbece8c74c9e74
cg021 <christiang021@gmail.com>
2020-11-17 01:21:43 -0600

    move main to bottom

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -20,49 +20,6 @@ var movieList;
 var addGroupName;
 var addGroupDescription;
 
-function Main() {
-
-    var _ud = localStorage.getItem('user_data');
-    var ud = JSON.parse(_ud);
-    userId = ud.id;
-    login = ud.login;
-    token = ud.token;
-
-    return(
-    <div>
-        <div id="mainDiv">
-            <div id="moviesDiv">
-            <button type="button" id="moviesTest" class="buttons" onClick={loadMovies}> Get Movies </button>
-            </div>
-            <div id="groupsDiv">
-                <button type="button" id="GroupTest" class="buttons" onClick={loadGroups}> Get Groups </button>
-            </div>
-        </div>
-        <div id="movieListDiv">
-        </div>
-        <div id="groupListDiv">
-        </div>
-        <div id="tempAddGroupDiv">
-            <form onSubmit={addGroup}>
-                <h3>Add Group</h3>
-                <div className="form-group">
-                    <label>Group Name</label>
-                    <input type="text" className="form-control" id="addGroupName" placeholder="Group Name" ref={(c) => addGroupName = c}/>
-                </div>
-
-                <div className="form-group">
-                    <label>Group Description</label>
-                    <input type="text" className="form-control" id="addGroupDescription" placeholder="Description" ref={(c) => addGroupDescription = c}/>
-                </div>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={addGroup}>Add Group</button>
-
-            </form>
-        </div>
-    </div>
-    );
-}
-
 // Takes what is in movieList and adds them the to page
 function createMovieList()
 {
@@ -168,5 +125,48 @@ const addGroup = async event => {
         return;
     }
 };
+
+function Main() {
+
+    var _ud = localStorage.getItem('user_data');
+    var ud = JSON.parse(_ud);
+    userId = ud.id;
+    login = ud.login;
+    token = ud.token;
+
+    return(
+    <div>
+        <div id="mainDiv">
+            <div id="moviesDiv">
+            <button type="button" id="moviesTest" class="buttons" onClick={loadMovies}> Get Movies </button>
+            </div>
+            <div id="groupsDiv">
+                <button type="button" id="GroupTest" class="buttons" onClick={loadGroups}> Get Groups </button>
+            </div>
+        </div>
+        <div id="movieListDiv">
+        </div>
+        <div id="groupListDiv">
+        </div>
+        <div id="tempAddGroupDiv">
+            <form onSubmit={addGroup}>
+                <h3>Add Group</h3>
+                <div className="form-group">
+                    <label>Group Name</label>
+                    <input type="text" className="form-control" id="addGroupName" placeholder="Group Name" ref={(c) => addGroupName = c}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Group Description</label>
+                    <input type="text" className="form-control" id="addGroupDescription" placeholder="Description" ref={(c) => addGroupDescription = c}/>
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={addGroup}>Add Group</button>
+
+            </form>
+        </div>
+    </div>
+    );
+}
 
 export default Main;
