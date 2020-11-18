f676b2374b8a3b9f024a298da95df90dbcb18797
cg021 <christiang021@gmail.com>
2020-11-17 18:48:41 -0600

    add headers for pages

---
+++ frontend/src/components/AccountHeader.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function AccountHeader() {
+    return(
+        <div>
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+            <Link className="navbar-brand" to={"/"}>Cinemates</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to={"/"}>Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to={"/register"}>Sign-up</Link>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+        </div>
+    );
+}
+
+export default AccountHeader;
