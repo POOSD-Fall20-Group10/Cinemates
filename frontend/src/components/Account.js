3418ddd00edfcfa2abe53ad261141d56a74ff779
StyxenStones <46583406+StyxenStones@users.noreply.github.com>
2020-11-17 19:47:20 -0500

    Added Elements to Account and gets passed info
    Added in account name and first and last name to the account page.
    These are fetched by getting the user's Id from localstorage 'current_account'
    Hit button to activate event. Will be removed to fire on page load.

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
+const app_name = 'cine-mates'
+function buildPath(route) {
+    if (process.env.NODE_ENV === 'production') {
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
+    }
+    else {
+        return 'http://localhost:5000/' + route;
+    }
+}
+
+var curAccount;
+var curAccountInfo;
+
 function Account() {
+
+    curAccount = localStorage.getItem('current_account');
+
+    const fetchAccount = async event => {
+        event.preventDefault();
+        var obj = {userID:curAccount};
+        var js = JSON.stringify(obj);
+
+        //API call
+        try {
+                const response = await fetch(buildPath('api/GetUserByID'), {
+                    method:'POST',body:js,headers:{
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                curAccountInfo = JSON.parse(await response.text()); // Response text
+                var div = document.getElementById("accountNameDiv"); // Account name Div
+                div.innerHTML = curAccountInfo.login; // Adds Account name to div
+                div = document.getElementById("accountFirstLast"); // Account F/L name Div
+                div.innerHTML = curAccountInfo.firstName + " " + curAccountInfo.lastName; // Adds F/L name to div
+            }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }
+    };
+
     return(
-        <div id="Account">
-        <h1 id="title">Account</h1>
+        <div>
+            <div id="Account">
+                <h1 id="title">Account</h1>
+                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={fetchAccount}>FetchAccount</button>
+            </div>
+            <div id="accountNameDiv">
+            </div>
+            <div id="accountFirstLast">
+            </div>
         </div>
    );
 }
