b20eac8260d4ada1c0b1e5b7aefa35793e4c5297
cg021 <christiang021@gmail.com>
2020-11-17 21:20:48 -0600

    add background

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -4,26 +4,45 @@ import './main.css';
 function RegisterHeader() {
     return(
         <div>
+        <div>&nbsp;&nbsp;&nbsp;</div>
         <h1 class="heading">Movies</h1>
         <div class="row">
             <div class="col">
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <div>&nbsp;</div>
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>           
             </div>
             <div class="col">
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <div>&nbsp;</div>
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>           
             </div>
             <div class="col">
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <div>&nbsp;</div>
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>           
             </div>
             <div class="col">
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <div>&nbsp;</div>
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>           
             </div>
             <div class="col">
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <div>&nbsp;</div>
+            <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>           
+            </div>
+        </div>
+
+        <h1 class="heading">Groups</h1>
+        <div class="row">
+            <div class="col">
+            <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>
+            <div>&nbsp;</div>
             <img src="https://i.pinimg.com/originals/96/a0/0d/96a00d42b0ff8f80b7cdf2926a211e47.jpg" width="200" height="200"/>           
             </div>
         </div>
