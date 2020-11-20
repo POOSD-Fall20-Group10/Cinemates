e4179d54c65565b61391732f50b19477cd33f755
cg021 <christiang021@gmail.com>
2020-11-19 22:39:05 -0600

    header sizing

--- frontend/src/components/GroupHeader.js
+++ frontend/src/components/GroupHeader.js
@@ -5,7 +5,7 @@ function GroupHeader() {
         <div>
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
             <div className="container">
-            <Link className="navbar-brand" to={"/"}>Cinemates</Link>
+            <Link className="nav-link" to={"/"}>Cinemates</Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
