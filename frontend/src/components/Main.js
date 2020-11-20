11809b28d705d860775f688e052e009d75a1601e
StyxenStones <46583406+StyxenStones@users.noreply.github.com>
2020-11-20 02:14:14 -0500

    Move MovieList Up

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -80,6 +80,7 @@ function Main() {
             <div id="mainDiv">
             </div>
             <GroupsList />
+            <MoviesList />
             <div id="tempAddGroupDiv">
             <div class="container">
                 <form onSubmit={addGroup}>
@@ -96,9 +97,6 @@ function Main() {
 
                     <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={addGroup}>Add Group</button>
                 </form>
-            </div>
-            <div id='moviesDiv' >
-            <MoviesList />
             </div>
         </div>
 
