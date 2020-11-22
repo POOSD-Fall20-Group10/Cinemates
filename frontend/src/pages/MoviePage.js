edf1db4c5c18c1ac62a4f9fc44e90a9ced098e4a
StyxenStones <46583406+StyxenStones@users.noreply.github.com>
2020-11-21 22:42:55 -0500

    Add files via upload

---
+++ frontend/src/pages/MoviePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Movie from '../components/Movie';
+
+const MoviePage = () =>
+{
+
+    return(
+      <div>
+        <Movie />
+      </div>
+    );
+};
+
+export default MoviePage;
