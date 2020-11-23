1052de93c26ca7764238407be873a740eb2bd05c
StyxenStones <46583406+StyxenStones@users.noreply.github.com>
2020-11-22 21:08:42 -0500

    Message Formatting, still doesn't display but page will load now

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -11,10 +11,46 @@ function buildPath(route) {
 }
 
 var chatLog;
+var chat;
 var token;
 var groupId;
 var messageToSend;
 var userId;
+var nameList;
+
+function createChat()
+{
+    var i;
+    nameList = new Array();
+    chat = "";
+
+    for(i = 0; i < chatLog.length; i++)
+    {
+        var obj = {userID:chatLog[i].senderID};
+        var js = JSON.stringify(obj);
+
+        // API Call
+        var xhr = new XMLHttpRequest();
+    	xhr.open("POST", buildPath('api/GetUserByID'), false);
+    	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try {
+                xhr.send(js);
+
+                var ret = JSON.parse(xhr.responseText);
+                nameList.push(ret.login);
+            }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }
+    }
+
+    for(i = 0; i < chatLog.length; i++)
+    {
+        chat.concat(nameList[i], ": ", chatLog[i].message, "\n");
+    }
+}
 
 const sendMessage = async event =>
 {
@@ -62,6 +98,7 @@ function Chat() {
 
             var ret = JSON.parse(xhr.responseText);
             chatLog = ret.messages;
+            createChat();
         }
     catch(e)
     {
@@ -72,7 +109,7 @@ function Chat() {
     return(
         <div id="chat">
             <h1 id="chatTitle">Chat</h1>
-            <h3 id="chatTitle">{chatLog}</h3>
+            <h3 id="chatTitle">{chat}</h3>
             <input type="text" class="form-control" id="messageToSend" placeholder="Write Your Message" ref={(c) => messageToSend = c}></input>
             <button type="submit" class="btn btn-success" onClick={sendMessage}>Send</button>
         </div>
